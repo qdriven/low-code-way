cc462159ec41307419e795ab61f7690cbb1f8d1d
Patrick <patrickwuke@163.com>
2022-10-31 11:35:55 +0800

    add load testing tools

--- qa-lowcode/pm/openproject/run.sh
+++ qa-lowcode/pm/openproject/run.sh
@@ -1,1 +1,8 @@
 docker-compose up -d
+
+
+docker run -it -p 8080:80 \
+  -e OPENPROJECT_SECRET_KEY_BASE=secret \
+  -e OPENPROJECT_HOST__NAME=localhost:8080 \
+  -e OPENPROJECT_HTTPS=false \
+  openproject/community:12
