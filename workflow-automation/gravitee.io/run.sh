ce367345511c09a52597c13288119171d721b6c8
Patrick <patrickwuke@163.com>
2022-12-14 11:03:09 +0800

    re-org folders

---
+++ workflow-automation/gravitee.io/run.sh
@@ -0,0 +1,16 @@
+mkdir -p ./gravitee/mongodb
+mkdir -p ./gravitee/mongodb/data
+mkdir -p ./gravitee/elasticsearch
+mkdir -p ./gravitee/elasticsearch/data
+mkdir -p ./gravitee/apim-gateway
+mkdir -p ./gravitee/apim-gateway/plugins
+mkdir -p ./gravitee/apim-gateway/logs
+mkdir -p ./gravitee/apim-management-api
+mkdir -p ./gravitee/apim-management-api/plugins
+mkdir -p ./gravitee/apim-management-api/logs
+mkdir -p ./gravitee/apim-management-ui
+mkdir -p ./gravitee/apim-management-ui/logs
+mkdir -p ./gravitee/apim-portal-ui
+mkdir -p ./gravitee/apim-portal-ui/logs
+
+docker-compose up 
---
+++ workflow-automation/kato/run.sh
@@ -0,0 +1,2 @@
+docker run --rm  -p 8081:8081  --name kaoto quay.io/kaotoio/standalone:test
+
