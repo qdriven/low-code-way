b692edfb2969b72ef7b1ba54849ff8563d373288
Patrick <patrickwuke@163.com>
2023-09-09 13:33:38 +0800

    remove .unused project

--- internal-tools/products/nocobase/qworkspace/packages/app/server/src/config/logger.ts
+++
@@ -1,6 +0,0 @@
-import { AppLoggerOptions } from '@nocobase/logger';
-
-export default {
-  transports: process.env.LOGGER_TRANSPORT || ['console', 'dailyRotateFile'],
-  level: process.env.LOGGER_LEVEL || (process.env.APP_ENV === 'development' ? 'debug' : 'info'),
-} as AppLoggerOptions;
--- internal-tools/products/nocobase/qworkspace/packages/app/server/src/config/plugins.ts
+++
@@ -1,3 +0,0 @@
-import { PluginConfiguration } from '@nocobase/server';
-
-export default ['nocobase'] as PluginConfiguration[];
